37177121cb725bfeb99dd3bcf4c278620a32d608
Drew Long <drewlong314@gmail.com>
2021-03-29 18:16:56 -0500

    finsished part 2

--- problems/02-tree-order.js
+++ problems/02-tree-order.js
@@ -2,10 +2,10 @@
 // and returns an array containing the values from a pre-order
 // traversal.
 function preOrderArray(root, finalarr = []) {
-    if(root === null) {
+    if (root === null) {
         return finalarr
-    } 
-    finalarr.push(root.val) 
+    }
+    finalarr.push(root.val)
     preOrderArray(root.left, finalarr)
 
     preOrderArray(root.right, finalarr)
@@ -16,8 +16,19 @@ function preOrderArray(root, finalarr = []) {
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from an in-order
 // traversal.
-function inOrderArray(root) {
+function inOrderArray(root, finalarr = []) {
     // Your code here
+    if (root === null) {
+        return finalarr;
+    }
+
+    inOrderArray(root.left, finalarr);
+    finalarr.push(root.val);
+    
+    inOrderArray(root.right, finalarr);
+
+    return finalarr
+
 }
 
 // Implement a function that takes the root of a Binary Tree
@@ -29,7 +40,7 @@ function postOrderArray(root) {
 
 
 module.exports = {
-  preOrderArray,
-  inOrderArray,
-  postOrderArray
+    preOrderArray,
+    inOrderArray,
+    postOrderArray
 };
