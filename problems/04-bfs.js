cd415e6aafdaa44498f439a737c118af441e3b06
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-03-30 11:32:50 -0500

    4

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -1,9 +1,55 @@
 // Implement a function that takes in the root of a
-// binary tree, traverses it in depth-first-search
+// binary tree, traverses it in breadth-first-search
 // order, and returns an array containing the values
 // in the order you visited them.
 function bfs(root){
-    // Your code here
+    if (root === null) {
+        return []
+    }
+
+    let queue = []
+    let finalarr = []
+    
+    queue.push(root.val)
+        
+        while(queue.length >= 1) {
+            console.log(root.left)
+            console.log(root.right)
+            finalarr.push(queue[0])
+            if(root.left !== null) {
+                queue.push(root.left)
+            } 
+            if(root.right !== null) {
+                queue.push(root.right)
+            }
+        }
+    
+    return finalarr
+    
+    // let queue = []
+
+    // queue.push(root.val)
+
+    // while(queue.length > 0) {
+    //     let current = queue[0]
+
+    //     if(current.left !== null) {
+    //         queue.push(current.left)
+    //     }
+    //     if(current.right !== null) {
+    //         queue.push(current.right)
+    //     }
+
+    //     queue.shift()
+    // }
+
+    // return queue
+
 }
+
+
+
+
+        
 
 module.exports = { bfs };
