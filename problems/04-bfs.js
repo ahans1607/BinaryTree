0618dbeb65279eedffd95d3b369e4d649a5b2165
Drew Long <drewlong314@gmail.com>
2021-03-30 12:04:35 -0500

    04

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -2,30 +2,76 @@
 // binary tree, traverses it in breadth-first-search
 // order, and returns an array containing the values
 // in the order you visited them.
-function bfs(root){
-    if (root === null) {
-        return []
-    }
+function bfs(root) {
 
-    let queue = []
-    let finalarr = []
-    
-    queue.push(root.val)
-        
-        while(queue.length >= 1) {
-            console.log(root.left)
-            console.log(root.right)
-            finalarr.push(queue[0])
-            if(root.left !== null) {
-                queue.push(root.left)
-            } 
-            if(root.right !== null) {
-                queue.push(root.right)
-            }
+    if (root === null) {
+        return [];
+    }
+    let queue = [root];
+    let finalArr = []
+    while (queue.length) {
+        let shifted = queue.shift()
+        finalArr.push(shifted.val)
+
+        if (shifted.left !== null) {
+            queue.push(shifted.left)
         }
-    
-    return finalarr
-    
+        if (shifted.right !== null) {
+            queue.push(shifted.right)
+        }
+        console.log(shifted, finalArr)
+    }
+    return finalArr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (root === null) {
+    //     return []
+    // }
+
+    // let queue = []
+    // let finalarr = []
+
+    // queue.push(root.val)
+
+    //     while(queue.length >= 1) {
+    //         console.log(root.left)
+    //         console.log(root.right)
+    //         finalarr.push(queue[0])
+    //         if(root.left !== null) {
+    //             queue.push(root.left)
+    //         }
+    //         if(root.right !== null) {
+    //             queue.push(root.right)
+    //         }
+    //     }
+
+    // return finalarr
+
     // let queue = []
 
     // queue.push(root.val)
@@ -50,6 +96,6 @@ function bfs(root){
 
 
 
-        
+
 
 module.exports = { bfs };
